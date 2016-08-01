184f804e9cba076f474ffcbcb50c266ada3de89a
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2016-08-01 12:13:03 +0200

    Experiment with lazy constructor for Maybe

--- base/src/main/scala/data/Maybe.scala
+++ base/src/main/scala/data/Maybe.scala
@@ -4,11 +4,15 @@ package data
 sealed abstract class Maybe[A] {
   final def fold[B](f: A => B, b: => B): B = this match {
     case Maybe.Just(a)  => f(a)
-    case _              => b
+    case Maybe.Empty    => b
+    case _              => this.asInstanceOf[Maybe.Lazy[A]].run.fold(f,b)
   }
 }
 
 object Maybe extends MaybeFunctions with MaybeInstances with MaybeSyntax {
   final private[data] case object Empty extends Maybe[Nothing]
   final case class Just[A](a: A) extends Maybe[A]
+  final private[data] class Lazy[A](a: => Maybe[A]) extends Maybe[A] {
+    def run = a
+  }
 }
--- base/src/main/scala/data/MaybeFunctions.scala
+++ base/src/main/scala/data/MaybeFunctions.scala
@@ -3,16 +3,14 @@ package data
 
 import typeclass.Liskov._
 
-import Maybe.{Empty, Just}
+import Maybe.{Empty, Just, Lazy}
 
 trait MaybeFunctions {
   def empty[A]: Maybe[A] = liftCvf[Nothing, A, Maybe](isa[Nothing, A]).apply(Empty)
   def just[A](a: A): Maybe[A] = Just(a)
+  def lazyMaybe[A](a: => Maybe[A]): Maybe[A] = new Lazy(a)
 
-  def maybe[A, B](n: B)(f: A => B): Maybe[A] => B = _ match {
-    case Empty    => n
-    case Just(x)  => f(x)
-  }
+  def maybe[A, B](n: B)(f: A => B): Maybe[A] => B = _.fold(f, n)
 
   def fromOption[A](oa: Option[A]): Maybe[A] = oa.fold[Maybe[A]](empty[A])(Just(_))
 }
